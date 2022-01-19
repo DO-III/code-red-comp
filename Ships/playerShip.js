4d40c803c53b5868e264748df06ba647a6335972
DO-III <davidiii@uw.edu>
2022-01-19 10:22:54 -0800

    Player appears. This took uncomfortably long.

--- Ships/playerShip.js
+++ Ships/playerShip.js
@@ -2,14 +2,24 @@ class PlayerShip {
     constructor(game) {
         //Initialize element.
         this.myGame = game;
+        this.imageAsset = ASSET_MANAGER.getAsset("./Ships/gfx/Player.png"); //Messy hardcode, fix later.
+
+        this.x = 100;
+        this.y = 100;
+
+        this.pointing = 0; //Zero is directly upwards.
 
     }
     
     draw(ctx) {
-
+        ctx.drawImage(this.imageAsset, this.x, this.y);
     }
 
     update() {
+        //The basic idea is that the player points in the direction they move in
+        //and shoots in the direction the arrow keys press.
+
+        //That means we need to rotate the graphic to match the WASD inputs.
 
     }
 }
--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const gameEngine = new GameEngine();
 
 const ASSET_MANAGER = new AssetManager();
+ASSET_MANAGER.queueDownload("./Ships/gfx/Player.png");
 //Add all images here.
 
 ASSET_MANAGER.downloadAll(() => {
@@ -8,6 +9,8 @@ ASSET_MANAGER.downloadAll(() => {
 	const ctx = canvas.getContext("2d");
 
 	gameEngine.init(ctx);
+
+	gameEngine.addEntity(new PlayerShip(gameEngine));
 
 	gameEngine.start();
 });
