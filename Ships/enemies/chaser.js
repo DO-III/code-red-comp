09840de9364e56f54501c99ebca1b788240933bd
DO-III <davidiii@uw.edu>
2022-01-26 10:14:50 -0800

    Stub for Chaser.

---
+++ Ships/enemies/chaser.js
@@ -0,0 +1,29 @@
+/*
+Chasers pursue the player relentlessly with some concept of velocity.
+
+They move stictly towards the player taking the most direct route possible.
+*/
+
+
+const CHASER_RADIUS = 5; //Size of Chaser bounding circle.
+const CHASER_MOVE_RATE = 1.5; //Speed at which Chaser moves.
+const CHASER_FRICTION = 0.9; //Rate at which Chaser loses speed. Lower = slower.
+
+
+class Chaser {
+    constructor(game) {
+        //Initialize element.
+        this.myGame = game;
+        //Size of bounding circle.
+        this.radius = 5;
+
+    }
+    
+    draw(ctx) {
+
+    }
+
+    update() {
+
+    }
+}
