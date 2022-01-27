67e3cc3c3456a526cf183d51e04d11cacbe00b14
DO-III <davidiii@uw.edu>
2022-01-26 16:25:44 -0800

    More progress on wanderer scripts.

--- Ships/enemies/wanderer.js
+++ Ships/enemies/wanderer.js
@@ -2,14 +2,14 @@
 Wanderers... wander.
 */
 
-const WANDERER_WIDTH = 38; //Should match graphic in final.
-const WANDERER_HEIGHT = 30; //Should match graphic in final.
+const WANDERER_WIDTH = 50; //Should match graphic in final.
+const WANDERER_HEIGHT = 50; //Should match graphic in final.
 
 const WGW_CENTER = WANDERER_WIDTH / 2; //Measures center of graphic, x-value.
 const WGH_CENTER = WANDERER_HEIGHT / 2; //Center of graphic, y-value.
 
-const WANDERER_RADIUS = 10; //Size of Wanderer bounding circle.
-const WANDERER_MOVE_RATE = 0.25; //Speed at which Wanderer moves.
+const WANDERER_RADIUS = 20; //Size of Wanderer bounding circle.
+const WANDERER_MOVE_RATE = 2.5; //Speed at which Wanderer moves.
 const WANDERER_FRICTION = 1; //Rate at which Chaser loses speed. Lower = slower.
 
 
@@ -18,36 +18,32 @@ class Wanderer {
         //Initialize element.
         this.myGame = game;
         this.imageAsset = ASSET_MANAGER.getAsset("./Ships/gfx/wanderer.png"); //Messy hardcode, fix later.
-        this.player = this.fetchPlayer(game);
-        console.log(this.player);
 
-        this.x = 100;
-        this.y = 100;
+        this.x = 200;
+        this.y = 200;
         this.dX = 0;
         this.dY = 0;
         this.xCenter = 0;
         this.yCenter = 0;
         this.updateCenter();
-        this.BoundingCircle = new BoundingCircle(CHASER_RADIUS, this.x, this.y);
+        this.BoundingCircle = new BoundingCircle(WANDERER_RADIUS, this.x, this.y);
 
-
-
-        this.playerX = 0;
-        this.playerY = 0;
+        //Pick a random direction and start moving.
+        this.angle = Math.random() * Math.PI * 2; //Random angle.
+        this.calcMovement();
         
 
     }
     
     draw(ctx) {
         var myCanvas = document.createElement('canvas');
-        myCanvas.width = CHASER_WIDTH;
-        myCanvas.height = CHASER_HEIGHT;
+        myCanvas.width = WANDERER_WIDTH;
+        myCanvas.height = WANDERER_HEIGHT;
         var myCtx = myCanvas.getContext('2d');
         myCtx.save();
-        myCtx.translate (CGW_CENTER, CGH_CENTER); //This should go to the center of the object.
-        this.angle = this.rotateHandle();
-        myCtx.rotate (this.angle);
-        myCtx.translate (-(CGW_CENTER), -(CGH_CENTER));
+        myCtx.translate (WGW_CENTER, WGH_CENTER); //This should go to the center of the object.
+        myCtx.rotate (this.angle );
+        myCtx.translate (-(WGW_CENTER), -(WGH_CENTER));
         myCtx.drawImage(this.imageAsset, 0, 0);
         myCtx.restore();
 
@@ -55,35 +51,16 @@ class Wanderer {
 
         //Debug to show bounding circle, keep out of final release.
         ctx.beginPath();
-        ctx.arc(this.xCenter, this.yCenter, CHASER_RADIUS, 0, 2 * Math.PI, false);
+        ctx.arc(this.xCenter, this.yCenter, WANDERER_RADIUS, 0, 2 * Math.PI, false);
         ctx.stroke();
     }
 
     update() {
-        //Get player's location.
-        this.playerX = this.player.xCenter;
-        this.playerY = this.player.yCenter;
         //Get current location.
         this.updateCenter();
-        this.calcMovement(this.xCenter, this.playerX, this.yCenter, this.playerY);
+        this.updateDirection();
         this.x += this.dX;
         this.y += this.dY;
-        this.x *= CHASER_FRICTION;
-        this.y *= CHASER_FRICTION;
-
-
-
-    }
-
-    /*
-    Fetch the player reference from the game manager.
-    */
-    fetchPlayer(game) {
-        var foundPlayer;
-        while(typeof foundPlayer === 'undefined') {
-            foundPlayer = game.entities.find(entity => entity instanceof PlayerShip);
-        }
-        return(foundPlayer);
     }
 
     /*
@@ -92,30 +69,55 @@ class Wanderer {
     For the bounding circle.
     */
     updateCenter() {
-        this.xCenter = this.x + CGW_CENTER;
-        this.yCenter = this.y + CGH_CENTER;
+        this.xCenter = this.x + WGW_CENTER;
+        this.yCenter = this.y + WGH_CENTER;
     }
+
+    collideLeft() {
+        return((this.xCenter - WANDERER_RADIUS) < 0)
+    }
+
+    collideRight() {
+        return((this.xCenter + WANDERER_RADIUS) > 600)
+    }
+
+    collideUp() {
+        return((this.yCenter - WANDERER_RADIUS) < 0)
+    }
+
+    collideDown() {
+        return((this.yCenter + WANDERER_RADIUS) > 600)
+    }
+
+    updateDirection() {
+        // collision with left or right walls
+        if (this.collideLeft() || this.collideRight()) {
+            this.dX *= -1;
+            
+        }
+        
+        // collision with top or bottom walls
+        if (this.collideUp() || this.collideDown()) {
+            this.dY *= -1;
+        }
+        this.rotate();
+        
+    }
+
+
 
     /*
-    Calculate the vector that will be used to move the bullets.
+    Calculate the vector that will be used to move the Wanderer.
 
-    Accomplished through the magic of polar coordinates.
+    This is much simpler than others.
     */
-    calcMovement(p1X, p2X, p1Y, p2Y) {
-        this.angle = Math.atan2(p2Y - p1Y, p2X - p1X);
-        this.dX += Math.cos(this.angle) * CHASER_MOVE_RATE;
-        this.dY += Math.sin(this.angle) * CHASER_MOVE_RATE;
+    calcMovement() {
+        this.dX += Math.cos(this.angle) * WANDERER_MOVE_RATE;
+        this.dY += Math.sin(this.angle) * WANDERER_MOVE_RATE;
     }
 
-    rotateHandle() {
-        if (this.player == null) {
-            return(0); //If player doesn't exist, don't rotate.
-        }
-
-        var dx = (this.playerX) - (this.x + CGW_CENTER); //Accounting for difference in center of thing.
-        var dy = (this.playerY) - (this.y + CGH_CENTER);
-
-        return (Math.atan2(dy, dx) + (Math.PI / 2));
+    rotate() {
+        this.angle = (Math.atan2(this.dY, this.dX) + (Math.PI / 2));
     }
 
 }
--- main.js
+++ main.js
@@ -3,6 +3,7 @@ const gameEngine = new GameEngine();
 const ASSET_MANAGER = new AssetManager();
 ASSET_MANAGER.queueDownload("./Ships/gfx/Player.png");
 ASSET_MANAGER.queueDownload("./Ships/gfx/chaser.png");
+ASSET_MANAGER.queueDownload("./Ships/gfx/wanderer.png");
 ASSET_MANAGER.queueDownload("./Ships/gfx/bullet.png");
 //Add all images here.
 
@@ -15,6 +16,7 @@ ASSET_MANAGER.downloadAll(() => {
 	gameEngine.addEntity(new PlayerShip(gameEngine));
 	//Messy hardcode. Should later have an entity that manages spawning.
 	gameEngine.addEntity(new Chaser(gameEngine));
+	gameEngine.addEntity(new Wanderer(gameEngine));
 	
 
 	gameEngine.start();
