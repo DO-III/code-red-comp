b72bf3ce33986581a1708d55363ed01bbc26f668
Sidise <87152496+Sidise@users.noreply.github.com>
2022-02-06 23:25:46 -0800

    Add files via upload
    New wanderer and chaser (rotation towards the player)

--- Ships/enemies/wanderer.js
+++ Ships/enemies/wanderer.js
@@ -11,12 +11,13 @@ const WGH_CENTER = WANDERER_HEIGHT / 2; //Center of graphic, y-value.
 const WANDERER_RADIUS = 20; //Size of Wanderer bounding circle.
 const WANDERER_MOVE_RATE = 2.5; //Speed at which Wanderer moves.
 const WANDERER_FRICTION = 1; //Rate at which Chaser loses speed. Lower = slower.
-
+const TIME_TO_SHOOT = .5;
 
 class Wanderer {
-    constructor(game) {
+    constructor(game,player) {
         //Initialize element.
         this.game = game;
+        this.player = player;
         this.imageAsset = ASSET_MANAGER.getAsset("./Ships/gfx/wanderer.png"); //Messy hardcode, fix later.
 
         this.x = 200;
@@ -27,23 +28,24 @@ class Wanderer {
         this.yCenter = 0;
         this.updateCenter();
         this.BoundingCircle = new BoundingCircle(WANDERER_RADIUS, this.xCenter, this.yCenter);
-
+        this.lastShoot = 0;
         //Pick a random direction and start moving.
-        this.angle = Math.random() * Math.PI * 2; //Random angle.
+        this.angle = this.rotateHandle();
         this.calcMovement();
-        
+
 
     }
-    
+
     draw(ctx) {
         var myCanvas = document.createElement('canvas');
         myCanvas.width = WANDERER_WIDTH;
         myCanvas.height = WANDERER_HEIGHT;
         var myCtx = myCanvas.getContext('2d');
         myCtx.save();
-        myCtx.translate (WGW_CENTER, WGH_CENTER); //This should go to the center of the object.
-        myCtx.rotate (this.angle );
-        myCtx.translate (-(WGW_CENTER), -(WGH_CENTER));
+        myCtx.translate(WGW_CENTER, WGH_CENTER); //This should go to the center of the object.
+        this.angle = this.rotateHandle();
+        myCtx.rotate (this.angle);
+        myCtx.translate(-(WGW_CENTER), -(WGH_CENTER));
         myCtx.drawImage(this.imageAsset, 0, 0);
         myCtx.restore();
 
@@ -56,12 +58,17 @@ class Wanderer {
     }
 
     update() {
-        //First off, have we been shot?
-        this.checkIfShot();
-
         //Get current location.
         this.updateCenter();
         this.updateDirection();
+        this.rotateHandle();
+        this.lastShoot += this.game.clockTick;
+
+        if(TIME_TO_SHOOT < this.lastShoot)
+        {
+            this.shoot();
+            this.lastShoot = 0;
+        }
         this.x += this.dX;
         this.y += this.dY;
     }
@@ -77,42 +84,48 @@ class Wanderer {
         this.BoundingCircle = new BoundingCircle(WANDERER_RADIUS, this.xCenter, this.yCenter);
     }
 
+    shoot() {
+        this.game.addEntity(new Bullet(this.game,
+            (this.x + WGW_CENTER),
+            (this.y + WGH_CENTER), player.x,player.y));
+    }
+
     collideLeft() {
-        return((this.xCenter - WANDERER_RADIUS) < 0)
+        return ((this.xCenter - WANDERER_RADIUS) < 0)
     }
 
     collideRight() {
-        return((this.xCenter + WANDERER_RADIUS) > 600)
+        return ((this.xCenter + WANDERER_RADIUS) > 600)
     }
 
     collideUp() {
-        return((this.yCenter - WANDERER_RADIUS) < 0)
+        return ((this.yCenter - WANDERER_RADIUS) < 0)
     }
 
     collideDown() {
-        return((this.yCenter + WANDERER_RADIUS) > 600)
+        return ((this.yCenter + WANDERER_RADIUS) > 600)
     }
 
     updateDirection() {
         // collision with left or right walls
         if (this.collideLeft() || this.collideRight()) {
             this.dX *= -1;
-            
+
         }
-        
+
         // collision with top or bottom walls
         if (this.collideUp() || this.collideDown()) {
             this.dY *= -1;
         }
-        this.rotate();
-        
+        // this.rotate();
+
     }
 
 
 
     /*
     Calculate the vector that will be used to move the Wanderer.
-
+    
     This is much simpler than others.
     */
     calcMovement() {
@@ -124,25 +137,16 @@ class Wanderer {
         this.angle = (Math.atan2(this.dY, this.dX) + (Math.PI / 2));
     }
 
-    /*
-    Has this enemy been shot?
-    
-    If so, this enemy is removed from the game world.
-    */
-    checkIfShot() {
-        var that = this;
-    
-        this.game.entities.forEach(function (entity) {
-            /*
-            Check if thing has bounding circle.
-            If so, make sure it's not the player.
-            If that's true, actually detect collision.
-            */
-            if(!(typeof entity.BoundingCircle === 'undefined') && (entity instanceof Bullet)
-              && entity.BoundingCircle && that.BoundingCircle.collide(entity.BoundingCircle)) {
-                entity.removeFromWorld = true;  
-                that.removeFromWorld = true;
-            }
-        })
+    rotateHandle() {
+        if (this.player == null) {
+            return(0); //If mouse isn't defined yet, don't try to rotate.
+                       //I know this is gross, bear with me.
+        }
+
+        var dx = (this.player.x) - (this.x + WGW_CENTER); //Accounting for difference in center of thing.
+        var dy = (this.player.y) - (this.y + WGH_CENTER);
+
+        return (Math.atan2(dy, dx) + (Math.PI / 2));
     }
+
 }
